f424589f4940448dfede2d3b4005a1af0291e92e
imran-salim <imransalim08@gmail.com>
2023-05-26 14:13:09 +1200

    refactor the API consumption

--- main.go
+++ main.go
@@ -15,31 +15,34 @@ type Response struct {
 	Quote string
 }
 
+const uri = "https://api.kanye.rest"
+
 func quoteKanyeWest(censor bool) string {
-	uri := "https://api.kanye.rest"
-
-	res, err := http.Get(uri)
-
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-
-	data, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var resObj Response
-	json.Unmarshal(data, &resObj)
-
-	quote := resObj.Quote
+	data := consumeAPI()
+	var respObj Response
+	json.Unmarshal(data, &respObj)
+	quote := respObj.Quote
 
 	if censor {
 		return goaway.Censor(quote)
 	}
 
 	return quote
+}
+
+func consumeAPI() []byte {
+	resp, err := http.Get(uri)
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return data
 }
 
 func main() {
