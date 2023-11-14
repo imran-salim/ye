d8015bd687a41c1bed91415eae4f740f5b5c7bff
i8abyte <imransalim08@gmail.com>
2023-11-14 13:40:41 +1300

    feat: create a custom profanity detector for an edge case of a false positive

--- client/quote_ye.go
+++ client/quote_ye.go
@@ -15,10 +15,19 @@ func QuoteYe(censored bool) string {
 	var respBody Response
 	json.Unmarshal(data, &respBody)
 	quote := "\"" + respBody.Quote + "\""
+	profanityDetector := CustomProfanityDetector()
 
-	if goaway.IsProfane(quote) && censored {
-		return goaway.Censor(quote)
+	if profanityDetector.IsProfane(quote) && censored {
+		return profanityDetector.Censor(quote)
 	}
 
 	return quote
+}
+
+func CustomProfanityDetector() *goaway.ProfanityDetector {
+	profanityDetector := goaway.NewProfanityDetector()
+	customFalsePositives := goaway.DefaultFalsePositives
+	customFalsePositives = append(customFalsePositives, "therapeutic")
+	profanityDetector.WithCustomDictionary(goaway.DefaultProfanities, customFalsePositives, goaway.DefaultFalseNegatives)
+	return profanityDetector
 }
